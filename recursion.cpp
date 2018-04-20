6c283967b2361a27f1b61efdb558102764ef40e3
geewizz246 <geewizz246@users.noreply.github.com>
2018-04-20 18:46:19 -0400

    Added further comments

--- recursion.cpp
+++ recursion.cpp
@@ -1,3 +1,4 @@
+//Program with a recursive function for detecting if string input is a palindrome
 #include <iostream>
 #include <string>
 
@@ -5,12 +6,15 @@ using namespace std;
 
 bool isPalindrome(string s)
 {
+	//if size is 0 assume a palindrome
 	if (s.size() == 0)
 		return true;
-		
+	
+	//similarly, if size is 1 assume a palindrome	
 	else if (s.size() == 1)
 		return true;
-		
+	
+	//if size > 1	
 	else{
 		int start = 0; //starting index
 		int end = s.size()-1; //ending index
@@ -25,18 +29,20 @@ bool isPalindrome(string s)
 			end--; 
 			len--;
 		}
-			
 		//cout << s[start] << s[end] << endl;
 		
-		if (s[start] != s[end])
+		if (s[start] != s[end]) //if first and last character don't match
 			return false;
+			
 		else if (start < end){
-			return isPalindrome(s.substr(start+1, len-2));
+			//send the middle of the current substring to the function again
+			return isPalindrome(s.substr(start+1, len-2)); 
 		}
 	}
 	
 	return true;		
 }//end isPalindrome
+
 
 int main()
 {
